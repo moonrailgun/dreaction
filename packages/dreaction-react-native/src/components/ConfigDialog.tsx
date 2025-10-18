00fc5739bc044551cc9fcde57eb9f7534b03bbd8
sumingcheng <cjwdyzdyplxjg@gmail.com>
2025-10-18 22:52:48 +0800

    fix: preserve port in ConfigDialog host input

--- packages/dreaction-react-native/src/components/ConfigDialog.tsx
+++ packages/dreaction-react-native/src/components/ConfigDialog.tsx
@@ -9,7 +9,7 @@ import {
 } from 'react-native';
 import { getHost } from '../helpers/getHost';
 import { dreaction } from '../dreaction';
-import { getHostFromUrl } from '../helpers/parseURL';
+import { getHostWithPortFromUrl } from '../helpers/parseURL';
 
 const LOCAL_CACHE_HOST_NAME = '__dreaction-react-native-host';
 
@@ -36,7 +36,7 @@ export const ConfigDialog: React.FC<ConfigDialogProps> = React.memo((props) => {
   }, []);
 
   const handleConfirm = () => {
-    const host = getHostFromUrl(inputValue);
+    const host = getHostWithPortFromUrl(inputValue);
     dreaction.asyncStorageHandler?.setItem(LOCAL_CACHE_HOST_NAME, host);
     onConfirm(host);
   };
